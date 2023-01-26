17aa05cfde5090f79461b66396583b3674f52a6e
Rafa Quintero <r.quintero@cesjuanpablosegundocadiz.es>
2023-01-26 23:06:02 +0100

    Esta sera la primera subida

---
+++ src/principal/Primeraprueba.java
@@ -0,0 +1,17 @@
+package principal;
+
+public class Primeraprueba {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int suma = 4;
+		int datos = 3;
+		System.out.println("Esto es un primera prueba");
+		
+		suma += datos;
+		
+		System.out.println("El total es : "+suma );
+	}
+
+}
